98bf21f9a236de15764d4251d9752ac8a8396a19
azam <aliazam@hotmail.co.uk>
2017-01-25 19:30:53 -0500

    this is made just for practice of git

---
+++ hello-world/script.js
@@ -0,0 +1,11 @@
+ReactDOM.render(
+    React.DOM.p({id:'blue'},'hi this is azam',
+    ' ali', ' zaidi', 
+    React.DOM.h1({style: {color: 'purple'}},'how are you' ),
+    React.DOM.div(null, React.DOM.p(null,'hel',
+    React.DOM.span({id:'bolder'},'l'),
+    'o')
+    )
+     ),
+    document.getElementById('div1')
+); 
